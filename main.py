1be6614f52f169ed1d93935c9204bd44769a56e9
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2024-11-05 13:52:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,19 @@ from sqlalchemy import or_
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 @app.on_event("startup")
 async def startup():
-    asyncio.create_task(start_bot())
+    await asyncio.create_task(start_bot())
+    
+    
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     with open("static/index.html") as f:
         return f.read()
+    
+
 @app.post("/register")
 def register(user: schemas.UserCreate, db: Session = Depends(auth.get_db)):
     try:
@@ -28,17 +34,21 @@ def register(user: schemas.UserCreate, db: Session = Depends(auth.get_db)):
         return {"detail": "User  created successfully"}
     except HTTPException as e:
         raise e
+    
 
 @app.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(auth.get_db)):
     user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials", headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials",
+                            headers={"WWW-Authenticate": "Bearer"})
     access_token = auth.create_access_token(data={"sub": user.username})
-    return {"token_type": "bearer","access_token": access_token }
+    return {"token_type": "bearer", "access_token": access_token}
+
 
 @app.post("/messages")
-async def create_message(message: schemas.MessageCreate, db: Session = Depends(auth.get_db), token: str = Depends(auth.oauth2_scheme)):
+async def create_message(message: schemas.MessageCreate, db: Session = Depends(auth.get_db),
+                         token: str = Depends(auth.oauth2_scheme)):
     try:
         payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
         username = payload.get("sub")
@@ -53,10 +63,10 @@ async def create_message(message: schemas.MessageCreate, db: Session = Depends(a
         if not recipient:
             raise HTTPException(status_code=404, detail="Recipient not found")
 
-        new_message = models.MessageUser (
+        new_message = models.MessageUser(
             sender_id=sender.id,
             recipient_id=recipient.id,
-            message=message.content )
+            message=message.content)
         db.add(new_message)
         db.commit()
         db.refresh(new_message)
@@ -64,9 +74,11 @@ async def create_message(message: schemas.MessageCreate, db: Session = Depends(a
         return {"detail": "Message created", "message_id": new_message.id}
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
 
 @app.get("/messages/history/{recipient_username}", response_model=List[schemas.MessageSch])
-async def get_message_history(recipient_username: str, token: str = Depends(auth.oauth2_scheme), db: Session = Depends(auth.get_db)):
+async def get_message_history(recipient_username: str, token: str = Depends(auth.oauth2_scheme),
+                              db: Session = Depends(auth.get_db)):
     try:
         payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
         username = payload.get("sub")
@@ -89,15 +101,17 @@ async def get_message_history(recipient_username: str, token: str = Depends(auth
         ).order_by(models.MessageUser.datetime).all()
         return [schemas.MessageSch(
            sender_id=message.sender_id,
-            recipient_id=message.recipient_id,
-            message=message.message,
-            timestamp=message.datetime
+           recipient_id=message.recipient_id,
+           message=message.message,
+           timestamp=message.datetime
         ) for message in messages]
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
 
 @app.get("/messages/history/{refresh}")
-async def get_refresh(recipient_username: str,token: str = Depends(auth.oauth2_scheme), db: Session = Depends(auth.get_db)):
+async def get_refresh(recipient_username: str, token: str = Depends(auth.oauth2_scheme),
+                      db: Session = Depends(auth.get_db)):
         sender = db.query(models.User).filter(models.User.token == token).first()
         recipient = db.query(models.User).filter(models.User.username == recipient_username).first()
         messages = db.query(models.MessageUser).filter(
@@ -113,6 +127,7 @@ async def get_refresh(recipient_username: str,token: str = Depends(auth.oauth2_s
             message=message.message,
             timestamp=message.datetime
         ) for message in messages]
+
 
 def root():
     return {"message": "Welcome to the chat service!"}
